63e2503e0793970bd5586cbb0c337564e55ffcba
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-13 15:23:46 +0530

    Update constants.py

--- config/constants.py
+++ config/constants.py
@@ -5,3 +5,36 @@ import random
 import string
 import tempfile
 import time
+from getpass import getuser
+
+date = time.strftime("%d%m%Y_%H%M%S")
+tmp = tempfile.gettempdir()
+
+class Constant:
+    
+    # Modes and Stuff for Script
+    # -----------------------------------
+    
+    QUIET_MODE = False
+    
+    
+    # Paths and Stuff
+    # -----------------------------------
+    
+    # Stuff needed to grabbing data
+    main_folder_name = os.getcwd()
+
+    # Stuff about Computer
+    username = getuser()
+    drive = u'C'
+    
+    profile = {
+        'HOMEDRIVE': u'{drive}:'.format(drive=drive),
+        'HOMEPATH': u'{drive}:\\Users\\{user}'.format(drive=drive, user=username),
+        'APPDATA': u'{drive}:\\Users\\{user}\\AppData\\Roaming\\'.format(drive=drive, user=username),
+        'USERPROFILE': u'{drive}:\\Users\\{user}\\'.format(drive=drive, user=username),
+        'COMPOSER_HOME': u'{drive}:\\Users\\{user}\\AppData\\Roaming\\Composer\\'.format(drive=drive, user=username),
+        'LOCALAPPDATA': u'{drive}:\\Users\\{user}\\AppData\\Local'.format(drive=drive, user=username),
+        'ALLUSERSPROFILE': u'{drive}:\\ProgramData'.format(drive=drive, user=username),
+    }
+    
