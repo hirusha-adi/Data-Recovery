ac4a018d2a1020a07c84286c2e694a77d745ed71
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-14 15:38:13 +0530

    .

--- config/constants.py
+++ config/constants.py
@@ -28,14 +28,3 @@ class Constant:
     username = getuser()
     drive = u'C'
     temp_dir = tempfile.gettempdir()
-    
-    profile = {
-        'HOMEDRIVE': u'{drive}:'.format(drive=drive),
-        'HOMEPATH': u'{drive}:\\Users\\{user}'.format(drive=drive, user=username),
-        'APPDATA': u'{drive}:\\Users\\{user}\\AppData\\Roaming\\'.format(drive=drive, user=username),
-        'USERPROFILE': u'{drive}:\\Users\\{user}\\'.format(drive=drive, user=username),
-        'COMPOSER_HOME': u'{drive}:\\Users\\{user}\\AppData\\Roaming\\Composer\\'.format(drive=drive, user=username),
-        'LOCALAPPDATA': u'{drive}:\\Users\\{user}\\AppData\\Local'.format(drive=drive, user=username),
-        'ALLUSERSPROFILE': u'{drive}:\\ProgramData'.format(drive=drive, user=username),
-    }
-    
