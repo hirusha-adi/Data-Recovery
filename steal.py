fba7050b945f88fe3a863160119cdb0eb7bed869
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-02-16 13:35:28 +0530

    default to printing everything

--- steal.py
+++ steal.py
@@ -4,15 +4,11 @@ from modules import *
 from config import Constant
 
 
-HELP = """
-update.exe 
-"""
-
 def parser():
     parser = argparse.ArgumentParser(description="Data Stealer | Another Data Recovery Tool | Built by @hirusha-adi")
-    parser.add_argument("--silent", "-s" , action="store_true", help="Silent Mode - No Console Output", default=True)
-    parser.add_argument("--verbose", "-v" , action="store_true", help="Verbose - Display everything that happens", default=False)
-    parser.add_argument("--log", "-l" , action="store_true", help="Log to file", default=False)
+    parser.add_argument("--silent", "-s" , action="store_true", help="Silent Mode - No Console Output", default=False)
+    parser.add_argument("--verbose", "-v" , action="store_true", help="Verbose - Display everything that happens", default=True)
+    parser.add_argument("--log", "-l" , action="store_true", help="Log to file", default=True)
     
     args = parser.parse_args()
     
