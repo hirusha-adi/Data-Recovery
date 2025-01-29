30780892408d1a1b2592144d4e5240a32c5955f3
hirusha-adi <hirushaadi@gmail.com>
2025-01-29 20:27:48 +0530

    add return type

--- recover.py
+++ recover.py
@@ -18,7 +18,7 @@ from modules import (
 @click.option("--verbose", "-v", is_flag=True, help="Verbose - Display all operations")
 @click.option("--log", "-l", is_flag=True, help="Enable logging to a file")
 @click.pass_context
-def cli(ctx: click.Context, silent: bool, verbose: bool, log: bool):
+def cli(ctx: click.Context, silent: bool, verbose: bool, log: bool) -> None:
     """Data Recovery | Built by @hirusha-adi"""
     ctx.ensure_object(dict)
 
@@ -36,7 +36,7 @@ def cli(ctx: click.Context, silent: bool, verbose: bool, log: bool):
 
 @cli.command(name="all", help="Recover everything")
 @click.pass_context
-def recover_all(ctx: click.Context):
+def recover_all(ctx: click.Context) -> None:
     """Recover all available data"""
     ctx.invoke(recover_browser, passwords=True, history=True, bookmarks=True)
     ctx.invoke(recover_network, wifi=True, info=True)
@@ -49,7 +49,7 @@ def recover_all(ctx: click.Context):
 @click.option("--history", "-h", is_flag=True, help="Recover browser history")
 @click.option("--bookmarks", "-b", is_flag=True, help="Recover browser bookmarks")
 @click.pass_context
-def recover_browser(ctx: click.Context, passwords: bool, history: bool, bookmarks: bool):
+def recover_browser(ctx: click.Context, passwords: bool, history: bool, bookmarks: bool) -> None:
     """Recover browser-related data"""
     if not (passwords or history or bookmarks):
         click.echo("No browser recovery options specified. Use --help for more info.")
@@ -67,7 +67,7 @@ def recover_browser(ctx: click.Context, passwords: bool, history: bool, bookmark
 @click.option("--wifi", "-nw", is_flag=True, help="Recover saved WiFi passwords")
 @click.option("--info", "-ni", is_flag=True, help="Recover network information")
 @click.pass_context
-def recover_network(ctx: click.Context, wifi: bool, info: bool):
+def recover_network(ctx: click.Context, wifi: bool, info: bool) -> None:
     """Recover network-related data"""
     if not (wifi or info):
         click.echo("No network recovery options specified. Use --help for more info.")
@@ -81,7 +81,7 @@ def recover_network(ctx: click.Context, wifi: bool, info: bool):
 
 @cli.command(name="system", help="Recover system information")
 @click.pass_context
-def recover_system(ctx: click.Context):
+def recover_system(ctx: click.Context) -> None:
     """Recover system information"""
     SystemInfoRecovery().run()
 
@@ -89,7 +89,7 @@ def recover_system(ctx: click.Context):
 @cli.command(name="apps", help="Recover application data")
 @click.option("--discord", "-ad", is_flag=True, help="Recover Discord tokens")
 @click.pass_context
-def recover_discord(ctx: click.Context, discord: bool):
+def recover_discord(ctx: click.Context, discord: bool) -> None:
     """Recover application-related data"""
     if discord:
         DiscordRecovery().run()
