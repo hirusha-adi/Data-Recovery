a88f6aca63107c74c6672a0dedf85e0dd749e80d
hirusha-adi <hirushaadi@gmail.com>
2025-02-03 23:16:56 +0530

    handle colors properly

--- config/colors.py
+++ config/colors.py
@@ -1,15 +1,23 @@
+# Special thanks to:
+#   https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
 
-# https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+import os
+import sys
 
 class Colors:
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
-    GREY    = '\33[90m'
+    """ANSI color codes, disabled if the terminal does not support colors."""
+    
+    # Detect if the terminal supports ANSI colors
+    _SUPPORTS_COLOR = sys.stdout.isatty() and (os.name != "nt" or "WT_SESSION" in os.environ or "TERM" in os.environ)
+
+    BLACK = '\033[30m' if _SUPPORTS_COLOR else ''
+    RED = '\033[31m' if _SUPPORTS_COLOR else ''
+    GREEN = '\033[32m' if _SUPPORTS_COLOR else ''
+    YELLOW = '\033[33m' if _SUPPORTS_COLOR else ''
+    BLUE = '\033[34m' if _SUPPORTS_COLOR else ''
+    MAGENTA = '\033[35m' if _SUPPORTS_COLOR else ''
+    CYAN = '\033[36m' if _SUPPORTS_COLOR else ''
+    WHITE = '\033[37m' if _SUPPORTS_COLOR else ''
+    GREY = '\33[90m' if _SUPPORTS_COLOR else ''
+    UNDERLINE = '\033[4m' if _SUPPORTS_COLOR else ''
+    RESET = '\033[0m' if _SUPPORTS_COLOR else ''
